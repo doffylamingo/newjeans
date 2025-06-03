0e310aadfc75f8f69db6e1ea14312f8d20934948
doffylamingo <102771222+doffylamingo@users.noreply.github.com>
2025-06-03 13:18:14 +0800

    feat: gallery page

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -167,3 +167,96 @@ export const albums = [
     ],
   },
 ];
+
+export const gallery = [
+  {
+    src: "https://kpopping.com/documents/b6/3/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-1.jpeg?v=d31db",
+    width: 800,
+    height: 534,
+  },
+  {
+    src: "https://kpopping.com/documents/21/3/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-2.jpeg?v=51064",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/9c/3/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-3.jpeg?v=51064",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/05/5/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-4.jpeg?v=d31db",
+    width: 800,
+    height: 534,
+  },
+  {
+    src: "https://kpopping.com/documents/ff/2/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-5.jpeg?v=51064",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/3a/3/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-6.jpeg?v=e993c",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/c3/0/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-8.jpeg?v=e993c",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/ef/5/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-9.jpeg?v=d31db",
+    width: 800,
+    height: 534,
+  },
+  {
+    src: "https://kpopping.com/documents/b3/3/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-1(1).jpeg?v=51064",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/f1/2/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-14.jpeg?v=dcce8",
+    width: 800,
+    height: 534,
+  },
+  {
+    src: "https://kpopping.com/documents/55/4/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-2(1).jpeg?v=e993c",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/87/2/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-3(2).jpeg?v=d31db",
+    width: 800,
+    height: 534,
+  },
+  {
+    src: "https://kpopping.com/documents/31/4/800/NewJeans-1st-Mini-Album-New-Jeans-Concept-Teasers-documents-3(1).jpeg?v=51064",
+    width: 800,
+    height: 1200,
+  },
+  {
+    src: "https://kpopping.com/documents/e1/0/800/NewJeans-Ditto-Concept-Teasers-documents-1.jpeg?v=4699e",
+    width: 800,
+    height: 534,
+  },
+  {
+    src: "https://kpopping.com/documents/9c/1/800/NewJeans-Ditto-Concept-Teasers-documents-2.jpeg?v=3bad8",
+    width: 800,
+    height: 1067,
+  },
+  {
+    src: "https://kpopping.com/documents/99/1/800/NewJeans-Ditto-Concept-Teasers-documents-3.jpeg?v=62410",
+    width: 800,
+    height: 1198,
+  },
+  {
+    src: "https://kpopping.com/documents/2a/4/800/NewJeans-Ditto-Concept-Teasers-documents-4.jpeg?v=c1154",
+    width: 800,
+    height: 534,
+  },
+  {
+    src: "https://kpopping.com/documents/6b/4/800/NewJeans-Ditto-Concept-Teasers-documents-6.jpeg?v=6c995",
+    width: 800,
+    height: 534,
+  },
+];
--- client/src/routes/gallery.tsx
+++ client/src/routes/gallery.tsx
@@ -1,9 +1,31 @@
+import Gallery from "@/components/Gallery";
+import Pagination from "@/components/Pagination";
+import usePagination from "@/hooks/usePagination";
+import { gallery } from "@/lib/constants";
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
 export const Route = createFileRoute("/gallery")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/gallery"!</div>;
+  const {
+    page: photoPage,
+    setPage: setPhotoPage,
+    totalPages,
+    paginatedItems: photos,
+  } = usePagination(gallery ?? [], 15);
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <main className="w-full min-h-screen max-w-[68rem] mx-auto px-4 md:px-0 py-6 md:py-8">
+      <Gallery photos={photos} index={index} setIndex={setIndex} />
+      <Pagination
+        currentPage={photoPage}
+        onPageChange={setPhotoPage}
+        totalPages={totalPages}
+      />
+    </main>
+  );
 }
